545c449a94e468ce05abfc417278700bfdd29787
aek676 <aek676@inlumine.ual.es>
2026-01-20 16:01:16 +0100

    feat: update CORS policy to allow requests from any origin in development environment

--- backend/Program.cs
+++ backend/Program.cs
@@ -28,7 +28,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
-    app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000"));
+    app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("*"));
 }
 
 app.UseHttpsRedirection();
